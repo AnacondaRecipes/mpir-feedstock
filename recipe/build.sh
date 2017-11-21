b230246e5447c1b87988fa3d412a3615002631be
Michael Sarahan <msarahan@gmail.com>
2017-11-21 13:19:34 -0600

    use cb3 features, remove 'fat' compilation that was breaking build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-source activate "${CONDA_DEFAULT_ENV}"
-
 chmod +x configure
-
-export PATH=$PATH:$PREFIX/bin
 
 if [ "$(uname)" == "Darwin" ];
 then
     ./configure --prefix=$PREFIX --enable-cxx --build=x86_64-apple-darwin
 else
-    ./configure --prefix=$PREFIX --enable-cxx --enable-fat
+    ./configure --prefix=$PREFIX --enable-cxx
 fi
 
 make
